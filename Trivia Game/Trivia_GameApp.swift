82b258331c0bcd18faeaf59a7bc67be9db53f6e0
pkritika <kritika.pant@bison.howard.edu>
2025-02-27 13:22:02 -0500

    Initial Commit

---
+++ Trivia Game/Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Kritika  on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
